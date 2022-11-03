ab1c64b50d19d84b2b7923d6ea7ad95958829cb6
HishamT <hisham.taweel@gmail.com>
2022-11-03 11:46:34 +0100

    create method to get the long URI

--- src/main/java/com/redi/shortener/controller/URLShortenerController.java
+++ src/main/java/com/redi/shortener/controller/URLShortenerController.java
@@ -2,9 +2,13 @@ package com.redi.shortener.controller;
 
 import com.redi.shortener.model.CreateShortURLRequest;
 import com.redi.shortener.model.CreateShortURLResponse;
+import com.redi.shortener.model.ExpandShortURLRequest;
+import com.redi.shortener.model.ExpandShortURLResponse;
 import com.redi.shortener.services.URLShortenerService;
 import java.net.MalformedURLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +22,10 @@ public class URLShortenerController {
       throws MalformedURLException {
     return urlShortenerService.create(request);
   }
+
+  @GetMapping("/shortener/expand")
+  public ExpandShortURLResponse expand(@RequestBody final ExpandShortURLRequest response){
+    return urlShortenerService.expand(response);
+  }
+
 }
