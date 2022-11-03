693afcbaef3ead86d3c28565dbec17965828ee72
HishamT <hisham.taweel@gmail.com>
2022-11-03 12:04:53 +0100

    change the Greeting.java model from record to class, so I can modify the id to customize RandomString. And fix the upper/lowerCase of name

--- src/main/java/com/redi/shortener/model/Greeting.java
+++ src/main/java/com/redi/shortener/model/Greeting.java
@@ -1,5 +1,28 @@
 package com.redi.shortener.model;
 
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public record Greeting(UUID id, String name) {}
+import org.apache.commons.lang3.RandomStringUtils;
+
+//public record Greeting(UUID id, String name) {}
+public class Greeting {
+    private final String id;
+    private final String name;
+    public Greeting(UUID id, String name){
+        this.id = RandomStringUtils.randomAlphanumeric(7);
+        this.name = Arrays.stream(name.split("\\s+"))
+                .map(t -> t.substring(0, 1).toUpperCase() + t.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
