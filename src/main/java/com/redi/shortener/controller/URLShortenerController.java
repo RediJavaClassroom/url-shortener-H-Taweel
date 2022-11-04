b03762a56b3f2636d7932902036634994f75bdca
HishamT <hisham.taweel@gmail.com>
2022-11-04 19:34:59 +0100

    format the code

--- src/main/java/com/redi/shortener/controller/URLShortenerController.java
+++ src/main/java/com/redi/shortener/controller/URLShortenerController.java
@@ -2,16 +2,11 @@ package com.redi.shortener.controller;
 
 import com.redi.shortener.model.CreateShortURLRequest;
 import com.redi.shortener.model.CreateShortURLResponse;
-import com.redi.shortener.model.ExpandShortURLRequest;
 import com.redi.shortener.model.ExpandShortURLResponse;
 import com.redi.shortener.services.URLShortenerService;
 import java.net.MalformedURLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class URLShortenerController {
