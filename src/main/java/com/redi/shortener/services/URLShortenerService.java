ec4ddf770d9e38a15a619db2e00063e95a21e7cf
HishamT <hisham.taweel@gmail.com>
2022-11-10 09:47:08 +0100

    modify the code to adapt with new DB

--- src/main/java/com/redi/shortener/services/URLShortenerService.java
+++ src/main/java/com/redi/shortener/services/URLShortenerService.java
@@ -3,20 +3,26 @@ package com.redi.shortener.services;
 import com.redi.shortener.model.CreateShortURLRequest;
 import com.redi.shortener.model.CreateShortURLResponse;
 import com.redi.shortener.model.ExpandShortURLResponse;
+import com.redi.shortener.persistence.URLShortened;
+import com.redi.shortener.repository.URLShortenerRepository;
+
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class URLShortenerService {
-  Map<String, URI> urlsDB = new HashMap<>();
 
-  public CreateShortURLResponse create(final CreateShortURLRequest request)
+  @Autowired
+  private URLShortenerRepository urlShortenerRepository;
+  public CreateShortURLResponse create(final CreateShortURLRequest request, LocalDate validUntil)
       throws MalformedURLException {
+
     String domainName = "http://localhost:8080/";
 
     //    if (urlsDB.containsValue(request.url())) {
@@ -24,17 +30,23 @@ public class URLShortenerService {
     //      URL shortURL = new URL(domainName + w);
     //      return new CreateShortURLResponse(shortURL);
     //    }
-    String identifier = RandomStringUtils.randomAlphanumeric(7);
-    URI shortURL = URI.create(domainName + identifier);
-    urlsDB.put(identifier, request.url());
-    return new CreateShortURLResponse(shortURL);
+    final String identifier = RandomStringUtils.randomAlphanumeric(7);
+    final URI shortURL = URI.create(domainName + identifier);
+    validUntil = LocalDate.now().plusDays(3);
+    final URLShortened urlShortened = new URLShortened();
+    urlShortened.setUrl(request.url().toString());
+    urlShortened.setKey(identifier);
+    urlShortenerRepository.save(urlShortened);
+
+    return new CreateShortURLResponse(shortURL, validUntil);
   }
 
-  public ExpandShortURLResponse expand(final String shortURLKey) {
-
+  public ExpandShortURLResponse expand(final String shortURLKey) throws URISyntaxException {
     //      if (urlsDB.containsKey(request.shortURLKey())){
     //          urlsDB.get(request.shortURLKey());
     //      }
-    return new ExpandShortURLResponse(urlsDB.get(shortURLKey));
+    final URLShortened longURL = urlShortenerRepository.getURLShortenedByKey(shortURLKey);
+    final URI response = new URI(longURL.getUrl());
+    return new ExpandShortURLResponse(response);
   }
 }
