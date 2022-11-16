6d180658b98ce35110bb53f8c8457b82509b2ea5
HishamT <hisham.taweel@gmail.com>
2022-11-16 22:06:14 +0100

    add validUntil

--- src/main/java/com/redi/shortener/persistence/URLShortened.java
+++ src/main/java/com/redi/shortener/persistence/URLShortened.java
@@ -1,44 +1,54 @@
 package com.redi.shortener.persistence;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table (name = "Url_Shortened")
 public class URLShortened {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "URL")
-    private String url;
-    private String key;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "URL")
+  private String url;
+  private String key;
+  private LocalDateTime validUntil;
 
-    public URLShortened(){}
+  public URLShortened() {}
 
-    public URLShortened(Long id, String url, String key) {
-        this.id = id;
-        this.url = url;
-        this.key = key;
-    }
+  public URLShortened(Long id, String url, String key, LocalDateTime validUntil) {
+    this.id = id;
+    this.url = url;
+    this.key = key;
+    this.validUntil = validUntil;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public String getUrl() {
+    return url;
+  }
 
-    public String getKey() {
-        return key;
-    }
+  public String getKey() {
+    return key;
+  }
 
+  public LocalDateTime getValidUntil() {
+    return validUntil;
+  }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+  public void setUrl(String url) {
+    this.url = url;
+  }
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public void setValidUntil(LocalDateTime validUntil) {
+    this.validUntil = validUntil;
+  }
 }
