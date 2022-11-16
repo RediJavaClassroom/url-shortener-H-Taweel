d8f61e0b01656128dd406d82f3a8878c9224f7f0
HishamT <hisham.taweel@gmail.com>
2022-11-16 23:50:31 +0100

    add UserServiceTest

--- src/test/java/com/redi/shortener/services/UserServiceTest.java
+++ src/test/java/com/redi/shortener/services/UserServiceTest.java
@@ -1,31 +1,30 @@
 package com.redi.shortener.services;
 
-import com.redi.shortener.model.User;
-import com.redi.shortener.model.UserRegistration;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
+import com.redi.shortener.model.User;
+import com.redi.shortener.model.UserRegistration;
 import org.junit.jupiter.api.Test;
 
 class UserServiceTest {
-    final UserService service = new UserService();
+  final UserService service = new UserService();
 
-    @Test
-    void shouldCreateUserWithRegistrationName() {
-        final UserRegistration registration = new UserRegistration("Joe", "joe@example.com");
-        final User user = service.create(registration);
+  @Test
+  void shouldCreateUserWithRegistrationName() {
+    final UserRegistration registration = new UserRegistration("Joe", "joe@example.com");
+    final User user = service.create(registration);
 
-        assertThat(user.name(), equalTo(registration.name()));
-    }
+    assertThat(user.name(), equalTo(registration.name()));
+  }
 
-    @Test
-    void shouldCreateRandomIds() {
-        final UserRegistration registration1 = new UserRegistration("Joe", "joe@example.com");
-        final UserRegistration registration2 = new UserRegistration("Paul", "paul@example.com");
-        final User user1 = service.create(registration1);
-        final User user2 = service.create(registration2);
-        assertThat(user1.id(), not(equalTo(user2.id())));
-    }
+  @Test
+  void shouldCreateRandomIds() {
+    final UserRegistration registration1 = new UserRegistration("Joe", "joe@example.com");
+    final UserRegistration registration2 = new UserRegistration("Paul", "paul@example.com");
+    final User user1 = service.create(registration1);
+    final User user2 = service.create(registration2);
+    assertThat(user1.id(), not(equalTo(user2.id())));
+  }
 }
